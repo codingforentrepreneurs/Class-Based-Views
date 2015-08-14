a3e8ce790930e802c26f1a38c13c57708db8d6f1
Justin Mitchel <jmitchel3@gmail.com>
2015-08-14 14:40:09 -0700

    3 - Intro & TemplateView

--- src/cbv/urls.py
+++ src/cbv/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     # Examples:
     url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
-    url(r'^about/$', 'cbv.views.about', name='about'),
+    #url(r'^about/$', 'cbv.views.about', name='about'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^team/$', TemplateView.as_view(template_name='team.html'), name='team'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- src/cbv/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def about(request):
-	return render(request, "about.html", {})
